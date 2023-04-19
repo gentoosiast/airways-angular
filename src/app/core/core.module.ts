c51f83426eebe868c6f74395fac905b0bfe1a501
Anna Perevedentseva <anna.pereved@gmail.com>
2023-04-20 00:54:17 +0200

    feat: add error-page markup

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [NotFoundPageComponent, FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule, TuiButtonModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
