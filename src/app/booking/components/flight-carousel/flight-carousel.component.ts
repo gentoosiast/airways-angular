5ce1b0a245bd84a81b25f432fd94aef75ed383fb
Sergey Koksharov <gentoosiastic@gmail.com>
2023-05-16 23:09:13 +0300

    fix: fix 'Flight' imports after refactoring

--- src/app/booking/components/flight-carousel/flight-carousel.component.ts
+++ src/app/booking/components/flight-carousel/flight-carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Flight } from '@booking/types/flight';
+import { Flight } from '@shared/types/flight';
 
 @Component({
   selector: 'air-flight-carousel',
