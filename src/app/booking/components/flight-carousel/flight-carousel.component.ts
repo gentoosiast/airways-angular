74f8d4df6d5d9057b0538ab696e2fb8eef585fa2
Sergey Koksharov <gentoosiastic@gmail.com>
2023-05-16 21:48:12 +0300

    refactor: use selected flight date as starting value for flight-carousel

--- src/app/booking/components/flight-carousel/flight-carousel.component.ts
+++ src/app/booking/components/flight-carousel/flight-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Flight } from '@booking/types/flight';
 
 @Component({
@@ -6,13 +6,17 @@ import { Flight } from '@booking/types/flight';
   templateUrl: './flight-carousel.component.html',
   styleUrls: ['./flight-carousel.component.scss'],
 })
-export class FlightCarouselComponent {
+export class FlightCarouselComponent implements OnInit {
   @Input() flights: Flight[] = [];
   @Input() isVisible = true;
   @Output() selectFlight = new EventEmitter<number>();
   displayItemsCount = 5; // TODO: value will depend on available screen space
   index = 0;
   selectedIndex?: number;
+
+  ngOnInit(): void {
+    this.index = Math.floor(this.flights.length / 2);
+  }
 
   onSelectFlight(index: number): void {
     this.selectedIndex = index;
