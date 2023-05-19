064cd414c08fd3d93dde8002eb65f6c4c7cfec4a
Sergey Koksharov <gentoosiastic@gmail.com>
2023-05-19 17:48:33 +0300

    refactor: update codebase to use new backend flight price data

--- src/app/shared/types/flight-price.ts
+++ src/app/shared/types/flight-price.ts
@@ -1,6 +1,18 @@
-export interface FlightPrice {
+interface Price {
   EUR: number;
   USD: number;
   RUB: number;
   PLN: number;
+}
+
+interface PriceComponents {
+  fare: Price;
+  tax: Price;
+}
+
+export interface FlightPrice {
+  total: Price;
+  adults: PriceComponents;
+  children: PriceComponents;
+  infants: PriceComponents;
 }
