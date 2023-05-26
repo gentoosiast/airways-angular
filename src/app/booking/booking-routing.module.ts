981e99ee30e21c01945caf0f149033e6ca4b746c
Sergey Koksharov <gentoosiastic@gmail.com>
2023-05-26 16:47:44 +0300

    feat: implement booking-passengers guard

--- src/app/booking/booking-routing.module.ts
+++ src/app/booking/booking-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookingFlightsPageComponent } from './pages/booking-flights-page/booking-flights-page.component';
 import { BookingPassengersPageComponent } from './pages/booking-passengers-page/booking-passengers-page.component';
 import { BookingSummaryPageComponent } from './pages/booking-summary-page/booking-summary-page.component';
+import { authGuardFn } from '@user/guards/auth.guard';
+import { bookingPassengersGuardFn } from './guards/booking-passengers.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'step-flights', pathMatch: 'full' },
   { path: 'step-flights', component: BookingFlightsPageComponent },
-  { path: 'step-passengers', component: BookingPassengersPageComponent },
+  {
+    path: 'step-passengers',
+    component: BookingPassengersPageComponent,
+    canMatch: [authGuardFn, bookingPassengersGuardFn],
+  },
   { path: 'step-summary', component: BookingSummaryPageComponent },
 ];
 
